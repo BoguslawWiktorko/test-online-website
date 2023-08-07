0d40410e674ac8e8669e900df990be0d9c36bd34
Boguslaw Wiktorko <wiktorkobw@gmail.com>
2023-08-07 14:03:38 +0200

    aktualizacja

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>Zenek</p>  
+        <>aktualizacja</>
+  
         <a
           className="App-link"
           href="https://reactjs.org"
